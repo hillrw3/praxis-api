7da08788afc2ed40372897ef73506670d068a916
Rob Hill <robert.hill0122@gmail.com>
2019-01-27 17:56:27 -0700

    Fix bug where progress report never returned habits as completed

--- app/models/progress_report.rb
+++ app/models/progress_report.rb
@@ -2,8 +2,8 @@ class ProgressReport
   def self.for_today
     habits = Habit.all.to_a
     todays_records = Record.where(completed_on: Date.today).to_a
-    record_ids = todays_records.map(&:id)
-    completed = habits.select {|h| record_ids.include?(h.id)}
+    record_habit_ids = todays_records.map(&:habit_id)
+    completed = habits.select {|h| record_habit_ids.include?(h.id)}
 
     {completed: completed, todo: habits - completed}
   end
